c63c86f9ce8f93d9d8414382462db8eec865d385
David Merrick <david@david-merrick.com>
2020-04-11 10:14:11 -0700

    Still working. Paring stuff down.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ application {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
     implementation("io.micronaut:micronaut-runtime:1.3.4")
     implementation("io.micronaut.aws:micronaut-function-aws-api-proxy:1.3.4")
     kapt(platform("io.micronaut:micronaut-bom:1.3.4"))
