57d9244c47abce88f9839036f65d0ef1067ce2ba
David Merrick <david@david-merrick.com>
2020-04-18 12:24:39 -0700

    Terraform is working

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -33,7 +33,7 @@ resource "aws_api_gateway_integration" "lambda" {
   resource_id = aws_api_gateway_method.proxy.resource_id
   http_method = aws_api_gateway_method.proxy.http_method
 
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
@@ -55,7 +55,7 @@ resource "aws_lambda_permission" "apigw" {
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*/*"
 }
 
 output "base_url" {
