ede6fd7a519e03b7329b8f3ea94d5f104675d6d1
David Merrick <david@david-merrick.com>
2020-05-01 18:51:36 -0700

    Added terraform for custom runtime

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -65,13 +65,13 @@ output "base_url" {
 # Lambda
 
 resource "aws_lambda_function" "lambda" {
-  filename = "../build/libs/quarantinebot.jar"
+  filename = "../build/native-image/function.zip"
   function_name = var.appName
   role = aws_iam_role.lambda-exec.arn
-  handler = "com.merricklabs.quarantinebot.StreamLambdaHandler"
-  runtime = "java11"
+  handler = "./bootstrap"
+  runtime = "provided"
   memory_size = 512
-  timeout = 15
+  timeout = 10
 
   environment {
     variables = {
