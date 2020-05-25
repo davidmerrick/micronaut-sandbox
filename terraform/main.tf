877fb8ce395b14a333653e387bb976f0f0fe8ea7
David Merrick <david@david-merrick.com>
2020-05-25 15:27:58 -0700

    Added terraform config for task definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@
 variable appName {}
 
 provider "aws" {
-  profile    = "terraform-sandbox"
+  profile    = "fargate"
   region     = "us-west-2"
 }
 
@@ -45,125 +45,12 @@ terraform {
   }
 }
 
-# API Gateway
+# ECS task definition
 
-resource "aws_api_gateway_rest_api" "example" {
-  name        = var.appName
-  description = "API for ${var.appName}"
-}
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    aws_api_gateway_integration.lambda
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  stage_name  = "prd"
-}
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # The "/*/*" portion grants access from any method on any resource
-  # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*/*"
-}
-
-output "base_url" {
-  value = aws_api_gateway_deployment.example.invoke_url
-}
-
-# Lambda
-
-resource "aws_lambda_function" "lambda" {
-  filename = "../build/native-image/function.zip"
-  function_name = var.appName
-  role = aws_iam_role.lambda-exec.arn
-  handler = "./bootstrap"
-  runtime = "provided"
-  memory_size = 256
-  timeout = 10
-
-  environment {
-    variables = {
-      SLACK_TOKEN = "banana"
-    }
-  }
-}
-
-resource "aws_iam_role" "lambda-exec" {
-  name = "${var.appName}-lambda"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
-
-# CloudWatch logging
-
-# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
-resource "aws_iam_policy" "lambda_logging" {
-  name        = "lambda_logging"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda-exec.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+resource "aws_ecs_task_definition" "quarantinebot" {
+  family                = "quarantinebot"
+  network_mode = "awsvpc"
+  cpu                   = 256
+  memory = 512
+  container_definitions = file("task-definitions/containers.json")
 }
